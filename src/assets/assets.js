184436506350d8e5c7f6feb298a01a8d276405b7
Zakaria Ennaqui <zakariaennaqui.pro@gmail.com>
2025-07-30 16:09:17 +0100

    c

---
+++ src/assets/assets.js
@@ -0,0 +1,59 @@
+import banner_img from './banner_img.png'
+import header_img from './header_img.jpg'
+import group_profiles from './group_profiles.png'
+import profile_pic from './profile_pic.png'
+import contact_image from './contact_image.webp'
+import about_image from './about_image.jpg'
+
+import add_icon from './add_icon.svg'
+import agenda_icon from './agenda_icon.svg'
+import cancel_icon from './cancel_icon.svg'
+import home_icon from './home_icon.svg'
+import people_icon from './people_icon.svg'
+import list_icon from './list_icon.svg'
+import tick_icon from './tick_icon.svg'
+import earning_icon from './earning_icon.svg'
+import dropdown_icon from './dropdown_icon.svg'
+import menu_icon from './menu_icon.svg'
+import cross_icon from './cross_icon.png'
+import chats_icon from './chats_icon.svg'
+import verified_icon from './verified_icon.svg'
+import arrow_icon from './arrow_icon.svg'
+import info_icon from './info_icon.svg'
+import upload_icon from './upload_icon.png'
+import upload_areap from './upload_area.png'
+import upload_areas from './upload_area.svg'
+
+import stripe_logo from './stripe_logo.png'
+import razorpay_logo from './razorpay_logo.png'
+import logo from './logo.svg'
+
+export const assets = {
+    logo,
+    stripe_logo,
+    razorpay_logo,
+    banner_img,
+    header_img,
+    group_profiles,
+    profile_pic,
+    contact_image,
+    upload_areap,
+    upload_areas,
+    about_image,
+    arrow_icon,
+    chats_icon,
+    verified_icon,
+    info_icon,
+    menu_icon,
+    cross_icon,
+    dropdown_icon,
+    upload_icon,
+    add_icon,
+    agenda_icon,
+    cancel_icon,
+    home_icon,
+    people_icon,
+    list_icon,
+    tick_icon,
+    earning_icon
+}
